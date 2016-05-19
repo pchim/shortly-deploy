19a5abb7ee7a423c932bb226f8da03a98bd843de
Peter Chim <pchim24@gmail.com>
2016-05-19 16:02:46 -0700

    Refactor models to use mongodb

--- app/models/user.js
+++ app/models/user.js
@@ -1,25 +1,30 @@
-var db = require('../config');
-var bcrypt = require('bcrypt-nodejs');
-var Promise = require('bluebird');
+let db = require('../config');
+let bcrypt = require('bcrypt-nodejs');
+let Promise = require('bluebird');
 
-var User = db.Model.extend({
-  tableName: 'users',
-  hasTimestamps: true,
-  initialize: function() {
-    this.on('creating', this.hashPassword);
-  },
-  comparePassword: function(attemptedPassword, callback) {
-    bcrypt.compare(attemptedPassword, this.get('password'), function(err, isMatch) {
-      callback(isMatch);
-    });
-  },
-  hashPassword: function() {
-    var cipher = Promise.promisify(bcrypt.hash);
-    return cipher(this.get('password'), null, null).bind(this)
-      .then(function(hash) {
-        this.set('password', hash);
-      });
-  }
+let userSchema = new Schema({
+  username: {type: String, unique: true},
+  password: String,
+},
+{
+  timestamps: true
 });
+
+userSchema.tableName = 'users';
+userSchema.hasTimestamps = true;
+userSchema.methods.comparePassword = function(attemptedPassword, callback) {
+  bcrypt.compare(attemptedPassword, this.get('password'), function(err, isMatch) {
+    callback(isMatch);
+  });
+userSchema.methods.hashPassword = function() {
+  var cipher = Promise.promisify(bcrypt.hash);
+  return cipher(this.get('password'), null, null).bind(this)
+    .then(function(hash) {
+      this.set('password', hash);
+    });
+};
+userSchema.queue('hashPassword');
+
+let User = db.model('User', userSchema);
 
 module.exports = User;
